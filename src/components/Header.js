3dfe90bb01f3cb48dbc4146c691c1c35c1d4048b
dsalato <d.salato@mail.ru>
2023-09-05 15:07:30 +0700

    Общая сумма товаров в корзине

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,10 +4,13 @@ import Order from "./Order";
 
 
 const showOrders = (props) => {
+    let summa = 0
+    props.orders.forEach(el=>summa += Number.parseFloat(el.price))
     return(<div>
         {props.orders.map(el => (
             <Order onDelete={props.onDelete} key={el.id} item={el} />
         ))}
+        <p className='summa'> Сумма: {summa.toFixed(1)}$</p>
     </div>)
 
 }
